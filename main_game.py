f684ef62e22081a87fe48f9565a3175c7fd6bc12
Ido Redid <Ido.redid@gmail.com>
2022-10-01 13:40:09 +0300

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -1,1 +1,12 @@
-print('Test')
+import pygame
+
+#Missions:
+
+#TODO --> Set up the pygame. Start to set up classes for each object and study this library's basics and advanced attributes and subjects.
+
+#Initialize the PyGame library 
+pygame.init()
+
+#Screen creation including sizes
+screen = pygame.display.set_mode((800, 600))
+print(f'PyGame is running')
