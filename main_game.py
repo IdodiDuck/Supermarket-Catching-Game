55a919bacacce784eb49fdf7b3761a1be5494d86
Ido Redid <Ido.redid@gmail.com>
2022-10-03 19:41:39 +0300

    Update - Part 2
    Added borders for the screen (Limits to where the character/object can move) and in addition added jumping functionality.

--- main_game.py
+++ main_game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #Missions:
 
@@ -7,17 +8,65 @@ import pygame
 #Initialize the PyGame library 
 pygame.init()
 
-WINDOW_HEIGHT = 800
-WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 500
 
 #Screen creation including sizes
 screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
 pygame.display.set_caption('Supermarket Game')
 
+# Characteristics
+
+x = 50
+y = 425
+width = 40
+height = 60
+vel = 5
+
+isJump = False
+jumpCount = 10
+
 #Program running loop
 run_program = True
+
 while run_program:
+    pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run_program = False
             print('Window closed by the user')
+
+    KEYS = pygame.key.get_pressed()
+
+    if KEYS[pygame.K_LEFT] and x > vel:
+        x -= vel
+
+    if KEYS[pygame.K_RIGHT] and x < 500 -  width - vel:
+        x += vel
+        
+    if not (isJump):
+        if KEYS[pygame.K_UP] and y > vel:
+            y -= vel
+
+        if KEYS[pygame.K_DOWN] and y < 500 - height - vel:
+            y += vel
+
+        if KEYS[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+
+        else:
+            isJump = False
+            jumpCount = 10
+
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
