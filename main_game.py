b8aa1791dc93d581e4b8e3cf17270ab575ff9f04
Ido Redid <Ido.redid@gmail.com>
2022-10-17 14:13:35 +0300

    Small Update
    Fixed animations pictures, Added Level and Score fonts texts to the game, fixed minor bugs and unnecessary code lines.

--- main_game.py
+++ main_game.py
@@ -1,12 +1,15 @@
-import pygame
+import pygame, random
 
 #Missions:
 
 #TODO --> Set up the pygame. Start to set up classes for each object and study this library's basics and advanced attributes and subjects.
 
-#Initialize the PyGame library 
-
+#Initialize the PyGame library and text
 pygame.init()
+
+pygame.font.init()
+leveltext = pygame.font.SysFont("bahnschrift", 60)
+scoretext = pygame.font.SysFont('bahnschrift', 40)
 
 WINDOW_HEIGHT = 750
 WINDOW_WIDTH = 630
@@ -15,11 +18,14 @@ WINDOW_WIDTH = 630
 screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
 pygame.display.set_caption('Supermarket Game')
 
-walkright_pic = pygame.image.load('ShoppingCartRight.png')
-walkleft_pic = pygame.image.load('ShoppingCartLeft.png') #D:\Ido.Files\Documents\GitHub\PyGame-Project\Shopping Cart Animations
- 
-# Characteristics
+score = 0
+level = 1
 
+#Animations pictures for the main-cart object
+walkright_pic = pygame.image.load('ShoppingCartRight.png')
+walkleft_pic = pygame.image.load('ShoppingCartLeft.png')
+
+# Classes and Attributes of all the objects
 class cart(object):
     def __init__(self, x, y, width, height):
         self.x = x
@@ -33,15 +39,16 @@ class cart(object):
 background = pygame.image.load('Game_SuperMarket_Banner.png')
 
 #Function which is Displaying the background
-
 def redrawGameWindow():
-    global walkCount
-
+    global walkCount, level, score
+    
     screen.blit(background, (0, 0)) #Draws the background
-    pygame.display.update() #Updates background and displays the background I downloaded.
+    leveltextTBD = leveltext.render(f'Level: {level}', 1, (0, 0, 0))
+    scoretextTBD = scoretext.render(f'Score: {score}', 1, (0, 0, 0))
 
-    if walkCount + 1 >= 27:
-        walkCount = 0
+    screen.blit(leveltextTBD, (2, 0))
+    screen.blit(scoretextTBD, (2, 68))
+    pygame.display.update() #Updates background and displays the background I downloaded.
 
     if left:
         screen.blit(walkleft_pic, (maincart.x, maincart.y))
@@ -56,14 +63,14 @@ def redrawGameWindow():
 
 run_program = True
 clock = pygame.time.Clock()
-maincart = cart(50, WINDOW_HEIGHT - walkleft_pic.get_height() -130, 50, 50)
+maincart = cart(50, WINDOW_HEIGHT - walkleft_pic.get_height() - 130, 50, 50)
 
 walkCount = 0 
 left, right = False, False
 
 #Main prorgam loop
 while run_program:
-    clock.tick(40)
+    clock.tick(40) #40 Frames Per Second (FPS)
     pygame.time.delay(100)
 
     for event in pygame.event.get():
@@ -73,23 +80,16 @@ while run_program:
 
     KEYS = pygame.key.get_pressed()
 
-    if KEYS[pygame.K_LEFT] and maincart.x > maincart.vel:
+    if KEYS[pygame.K_LEFT] and maincart.x > maincart.vel -  maincart.width + 39: #Left Movement
         maincart.x -= maincart.vel
         left = True
         right = False
 
-    elif KEYS[pygame.K_RIGHT] and maincart.x < 570 - maincart.width - maincart.vel:
+    elif KEYS[pygame.K_RIGHT] and maincart.x < 600 - maincart.width - maincart.vel: #Right Movement
         maincart.x += maincart.vel
         left = False
         right = True
-
-    # else:
-    #     right = False
-    #     left = False
-    #     walkCount = 0
-        
-    redrawGameWindow()  
+   
+    redrawGameWindow()
 
     pygame.display.update()
-    
-pygame.quit()
