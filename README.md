c3467898ac638c9677d930f7e3d58e01fcfa765b
Ido Redid <Ido.redid@gmail.com>
2023-01-01 19:17:18 +0200

    Main Update 1.2
    - Fixed bugs
    - Cleaned the code
    - Added documentations
    - Added function main, fixed the logic of the code.
    - Added music to the game including a music.mp3 file
    - Edited and added details about the game and myself in README.md file

--- README.md
+++ README.md
@@ -5,26 +5,38 @@
 ```Still in progress..```
 ---
 ## **What is this Repository about?:**
-The project is about a supermarket game, the goal of the game is to catch the falling food objects, reach points and reach high levels. The velocity of the objects will get higher when going to higher levels. If a food object reaches the floor 3 times, you're out.
+The project is about a supermarket game, the goal of the game is to catch the falling food objects, reach points and reach high levels. The velocity of the objects will get higher when going to higher levels. If a food object reaches the floor 3 times, that's a game-over.
 
-Using abilities such as:
-Subjects under...
+## **How to play?**
+
+Move the maincart of the game by using the right and left arrow keys/buttons in the keyboard located right to the shift/enter buttons.
+
+
+## **Knowledge used in the repository:**
+
+- `Pygame -> Library functions, methods, classes and importations.`
+- `OOP -> Object Oriented Programming -> Classes, Instances, Attributes, Methods, Inheritance...`
+- `Functional Programming -> Functions, global and local variables, the main function...`
+- `Flow Control -> While loop, for loop, if statements, else statements, elif statements...`
+- `Mathematical functions & calculations and visual imagination...`
 
 ---
 ## **About me:**
-``My name is Ido, I'm a programming enthusaistic``
-```For any communication, my discord --> IdoRedid123#7761```
+``My name is Ido, I'm a Programming & Computers Sciencesenthusaistic``
+```For any communicationwith me , here's my discord --> IdoRedid123#7761```
 ---
 ---
 ## **My languages:**
 The languages I know are:
 - `Python`
 - `Go`
-- `SQL - (MySQL)`
+- `SQL using MySQL`
 - `MarkDown`
 
 ## In this project I use only Python and MarkDown, in addition I use Pygame and Random libraries
 ---
-## *More coming soon...*
+## **Credits:**
+- `Programmer & Designer - idoredid123`
+- `Pictures and contribution - A47_22`
 
 
--- main_game.py
+++ main_game.py
@@ -7,15 +7,17 @@ pygame.font.init()
 leveltext = pygame.font.SysFont("bahnschrift", 60)
 scoretext = pygame.font.SysFont("bahnschrift", 40)
 livestext = pygame.font.SysFont("bahnschrift", 45)
-foody_text = pygame.font.SysFont("bahnschrift", 45)
 
 WINDOW_HEIGHT = 750
 WINDOW_WIDTH = 630
 
-#Screen creation including sizes
+#Setting up the game
 screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
 pygame.display.set_caption('Supermarket Game')
 background = pygame.image.load('Game_SuperMarket_Banner.png')
+
+music = pygame.mixer.music.load("music.mp3")
+pygame.mixer.music.play(-1)
 
 #Animations pictures for the main-cart object
 walkright_pic = pygame.image.load('ShoppingCartRight.png')
@@ -35,98 +37,141 @@ class cart(object):
         self.vel = vel
 
 class bread(object):
+    '''
+    The bread food object class, one of the options of the random food falling objects. Bread worths 5 points in the score
+    '''
     pic = pygame.image.load('Bread_Obj.png')
-    def __init__(self, x, y, respawn):
+    def __init__(self, x, y, respawn, vel):
         self.x = x
         self.y = y
         self.respawn = respawn
         self.pts = 5
-        self.vel = 1
+        self.vel = vel
     
     def draw(self):
+        '''
+        A function which draws the instance of the bread food object to the screen every frame in coordinates (self.x, self.y)
+        '''
         screen.blit(bread.pic, (self.x, self.y))
         
 class drinks(object):
+    '''
+    The drinks food object class, one of the options of the random food falling objects. Drinks worths 10 points in the score
+    '''
     pic = pygame.image.load('Drinks_Obj.png')
-    def __init__(self, x, y, respawn):
+    def __init__(self, x, y, respawn, vel):
         self.x = x
         self.y = y
         self.respawn = respawn
         self.pts = 10
-        self.vel = 1
+        self.vel = vel
 
     def draw(self):
+        '''
+        A function which draws the instance of the drinks food object to the screen every frame in coordinates (self.x, self.y)
+        '''
         screen.blit(drinks.pic, (self.x, self.y))
 
 class eggs(object):
+    '''
+    The eggs cartoon food object class, one of the options of the random food falling objects. Eggs worths 5 points in the score
+    '''
     pic = pygame.image.load('Eggs_Obj.png')
-    def __init__(self, x, y, respawn):
+    def __init__(self, x, y, respawn, vel):
         self.x = x
         self.y = y
         self.respawn = respawn
         self.pts = 5
-        self.vel = 1
+        self.vel = vel
 
     def draw(self):
+        '''
+        A function which draws the instance of the eggs food object to the screen every frame in coordinates (self.x, self.y)
+        '''
         screen.blit(eggs.pic, (self.x, self.y))
 
 class milk(object):
+    '''
+    The milk food object class, one of the options of the random food falling objects. Milk worths 5 points in the score
+    '''
     pic = pygame.image.load('Milk_Obj.png')
-    def __init__(self, x, y, respawn):
+    def __init__(self, x, y, respawn, vel):
         self.x = x
         self.y = y
         self.respawn = respawn
         self.pts = 5
-        self.vel = 1
+        self.vel = vel
 
     def draw(self):
+        '''
+        A function which draws the instance of the milk food object to the screen every frame in coordinates (self.x, self.y)
+        '''
         screen.blit(milk.pic, (self.x, self.y))
 
 class veg(object):
+    '''
+    The vegetables food object class, one of the options of the random food falling objects. Vegetables worths 5 points in the score
+    '''
     pic = pygame.image.load('Vegetables_Obj.png')
-    def __init__(self, x, y, respawn):
+    def __init__(self, x, y, respawn, vel):
         self.x = x
         self.y = y
         self.respawn = respawn
         self.pts = 5
-        self.vel = 1
+        self.vel = vel
 
     def draw(self):
+        '''
+        A function which draws the instance of the vegetables food object to the screen every frame in coordinates (self.x, self.y)
+        '''
         screen.blit(veg.pic, (self.x, self.y))
 
 class meat(object):
+    '''
+    The meat food object class, one of the options of the random food falling objects. Meat worths 10 points in the score
+    '''
     pic = pygame.image.load('Meat_Obj.png')
-    def __init__(self, x, y, respawn):
+    def __init__(self, x, y, respawn, vel):
         self.x = x
         self.y = y
         self.respawn = respawn
         self.pts = 10
-        self.vel = 1
+        self.vel = vel
 
     def draw(self):
+        '''
+        A function which draws the instance of the meat food object to the screen every frame in coordinates (self.x, self.y)
+        '''
         screen.blit(meat.pic, (self.x, self.y))
 
 class goldencandy(object):
+    '''
+    The Golden Candy food object class, one of the options of the random food falling objects but the most valueable one. Golden Candy worths 100 points in the score
+    '''
     pic = pygame.image.load('GoldenCandy_Obj.png')
-    def __init__(self, x, y, respawn):
+    def __init__(self, x, y, respawn, vel):
         self.x = x
         self.y = y
         self.respawn = respawn
         self.pts = 100
-        self.vel = 1
+        self.vel = vel 
         
-
     def draw(self):
+        '''
+        A function which draws the instance of the golden candy food object to the screen every frame in coordinates (self.x, self.y)
+        '''
         screen.blit(goldencandy.pic, (self.x, self.y))
-  
+
 def generate_food():
+    '''
+    A function which generates one of the food object classes and returns an instance of it to the main core function of the program.
+    '''
     food_tup = (bread, drinks, eggs, meat, veg, milk, goldencandy)
 
     current_food = random.choice(food_tup)
-    current_food_instance = current_food(random.choice(range(10, WINDOW_WIDTH, 10)), current_food.pic.get_height(), False)
-    return current_food_instance
 
-current_food_instance = generate_food()
+    current_food_instance = current_food(random.choice(range(10, WINDOW_WIDTH, 10)), current_food.pic.get_height(), False, current_food_vel)
+    return current_food_instance
 
 score = 0
 level = 1
@@ -135,9 +180,15 @@ level_requirement = 10
 
 cart_height = 421
 
+current_food_vel = 1
+current_food_instance = generate_food()
+
 #Function which is drawing the images in the game and updates the display in every frame
 def redrawGameWindow():
-    global screen, level, score, lives, level_requirement, current_food_instance
+    '''
+    A function which redraws the game window in each frame, draws the level, scores and lives texts. Additionaly it draws the maincart and the random falling food objects including speed control of the maincart and food-objects.
+    '''
+    global screen, level, score, lives, level_requirement, current_food_vel, current_food_instance
 
     screen.blit(background, (0, 0)) #Draws the background
     leveltextTBD = leveltext.render(f'Level: {level}', 1, (0, 0, 0))
@@ -157,14 +208,13 @@ def redrawGameWindow():
 
             if score >= level_requirement:
                 level += 1
-                current_food_instance.vel *= 10
-
-                level_requirement += 10
+                level_requirement *= 1.5
+                current_food_vel += 0.10
 
             current_food_instance = generate_food()
 
         else:
-            current_food_instance.y += 4 * current_food_instance.vel
+            current_food_instance.y += 3 * current_food_instance.vel
 
     elif current_food_instance.y >= 510:
         # current_food_instance.draw()
@@ -172,7 +222,7 @@ def redrawGameWindow():
         current_food_instance.respawn = True
         current_food_instance = generate_food()
     else:
-        current_food_instance.y += 4 * current_food_instance.vel
+        current_food_instance.y += 3 * current_food_instance.vel
         
     if left:
         screen.blit(walkleft_pic, (maincart.x, maincart.y))
@@ -191,32 +241,42 @@ maincart = cart(50, WINDOW_HEIGHT - walkleft_pic.get_height() - 130, 60, 60, 20)
 
 left, right = False, False
 
-#Main prorgam loop
-while run_program:
-    clock.tick(40) #40 Frames Per Second (FPS)
-    pygame.time.delay(100)
+def main():
+    '''
+    The main function of the program, includes the program flow and running including FPS and drawing functions.
+    '''
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    global run_program, clock, maincart, right, left
+
+    #Main prorgam loop
+    while run_program:
+        clock.tick(40) #40 Frames Per Second (FPS)
+        pygame.time.delay(100)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run_program = False
+                print(f'WINDOW CLOSED BY THE USER\nYour score is {score}')
+
+        if lives == 0:
+            print(f"GAME OVER!\nYour score is {score}")
             run_program = False
-            print('WINDOW CLOSED BY THE USER')
 
-    if lives == 0:
-        print("GAME OVER!")
-        run_program = False
+        KEYS = pygame.key.get_pressed()
 
-    KEYS = pygame.key.get_pressed()
+        if KEYS[pygame.K_LEFT] and maincart.x > maincart.vel -  maincart.width + 39: #Left Movement
+            maincart.x -= maincart.vel
+            left = True
+            right = False
 
-    if KEYS[pygame.K_LEFT] and maincart.x > maincart.vel -  maincart.width + 39: #Left Movement
-        maincart.x -= maincart.vel
-        left = True
-        right = False
+        elif KEYS[pygame.K_RIGHT] and maincart.x < 600 - maincart.width - maincart.vel: #Right Movement
+            maincart.x += maincart.vel
+            left = False
+            right = True
 
-    elif KEYS[pygame.K_RIGHT] and maincart.x < 600 - maincart.width - maincart.vel: #Right Movement
-        maincart.x += maincart.vel
-        left = False
-        right = True
+        redrawGameWindow()
 
-    redrawGameWindow()
+        pygame.display.update()
 
-    pygame.display.update()
+if __name__ == '__main__':
+    main()
